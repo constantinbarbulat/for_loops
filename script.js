0e3722ac86b36cac86ff96488fffee69be5d86bc
kaby <constantinbarbulat@gmail.com>
2023-03-20 08:52:19 +0100

    0.2

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 "use strict";
 
+//=============Loop_for===========
+/*
+for (let i = 0; i <= 5; i++) {
+   console.log(i);
+}*/
+//================================
 
+//=============Loop_for x2===========
+/*
+for (let i = 0; i <= 3; i++) {
+    console.log(i);
+    
 
+        for (let j = -1; j >= -3; j--) {
+            console.log(j);
+        }
+ 
+ }
+ */
+ //=======Asterix piramid===========
+ let result = '';
+ let lenght = 7;
+ for (let i = 0; i < lenght; i++) {
+    
+    for (let j = 0; j < i; j++) {
+        result += "*";
+    }
+    result += '\n';
+ }
+ console.log(result);
+ 
+  //================================
